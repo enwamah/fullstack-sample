871e0e0793252a58eb4be64a46d4627ab8d9225d
Emmanuel Nwamah <emmanuelnwamah@gmail.com>
2025-06-24 18:46:01 -0400

    Renamed services

--- shop/src/app/services/category.service.ts
+++ shop/src/app/services/category.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class Category {
+export class CategoryService {
   private url = '/api/categories';
   constructor(private http: HttpClient) { }
 
