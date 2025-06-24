31f57bf29ca3b228b195b0115292c506b1f9adaf
Emmanuel Nwamah <emmanuelnwamah@gmail.com>
2025-06-24 18:38:24 -0400

    Add product form and list routes

--- shop/src/app/app.routes.ts
+++ shop/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductsList } from './components/products-list/products-list';
+import { ProductForm } from './components/product-form/product-form';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+  { path: 'products', component: ProductsList },
+  { path: 'product-form', component: ProductForm },
+];
