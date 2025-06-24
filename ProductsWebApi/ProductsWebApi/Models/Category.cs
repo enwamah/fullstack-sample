551012021dbcf1ad74d9fee7458f3d682637ed43
Emmanuel Nwamah <emmanuelnwamah@gmail.com>
2025-06-24 11:33:02 -0400

    Add category model

---
+++ ProductsWebApi/ProductsWebApi/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductsWebApi.Models
+{
+    public class Category
+    {
+        public int Id { get; init; }
+        public string Name { get; init; } = string.Empty;
+        public string Description { get; init; } = string.Empty;
+
+        public List<Product> Products { get; init; } = [];
+    }
+}
