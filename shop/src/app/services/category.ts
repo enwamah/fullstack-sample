bb3026d8503e3ecb6e8eb20dc09b7255c7284cdd
Emmanuel Nwamah <emmanuelnwamah@gmail.com>
2025-06-24 18:42:43 -0400

    Add implementation of product and category services

--- shop/src/app/services/category.ts
+++ shop/src/app/services/category.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Category } from '../models/category';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Category {
+  private url = '/api/categories';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.url);
+  }
 }
--- shop/src/app/services/product.ts
+++ shop/src/app/services/product.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product, CreateProductDto } from '../models/product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Product {
 
-  constructor() { }
+  private url = '/api/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.url);
+  }
+
+  createProduct(product: CreateProductDto): Observable<Product> {
+    return this.http.post<Product>(this.url, product);
+  }
 }
