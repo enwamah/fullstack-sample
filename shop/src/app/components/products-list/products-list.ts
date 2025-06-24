5cdf3996304ffef995a85c4b2ca163b5ab4faf3f
Emmanuel Nwamah <emmanuelnwamah@gmail.com>
2025-06-24 18:57:47 -0400

    Add implementation of products list component

--- shop/src/app/components/products-list/products-list.ts
+++ shop/src/app/components/products-list/products-list.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { CategoryService } from '../../services/category.service';
+import { Product } from '../../models/product';
+import { Category } from '../../models/category';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products-list.html',
   styleUrl: './products-list.css'
 })
-export class ProductsList {
 
+export class ProductsList {
+  products: Product[] = [];
+  categories: Category[] = [];
+  categoryMap = new Map<number, string>();
+
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService
+  ) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe({
+      next: (data: Product[]) => {
+        this.products = data;
+      },
+      error: (err: any) => {
+        console.error('Failed to load products', err);
+        alert('Failed to load products');
+      }
+    });
+
+
+    this.categoryService.getCategories().subscribe({
+      next: (data: Category[]) => {
+        this.categories = data;
+        this.categoryMap = new Map(this.categories.map(c => [c.id, c.name]));
+      },
+      error: (err: any) => {
+        console.error('Failed to load categories', err);
+        alert('Failed to load categories');
+      }
+    });
+  }
+
+  getCategoryNameById(id: number): string {
+    return this.categoryMap.get(id) ?? 'Unknown';
+  }
 }
